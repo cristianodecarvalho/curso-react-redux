39a98c3f5b7e1f59b682682151d5cb072e4b7057
Cristiano de Carvalho Martins Filho <cristianfilho18@gmail.com>
2022-04-18 21:22:45 -0300

    Rendering children components using props.children

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css"
 import React from 'react';
 
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro';
@@ -12,6 +14,14 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#5 Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Cristiano" />
+                    <FamiliaMembro nome="Italo" />
+                    <FamiliaMembro nome="Caue" />
+                </Familia>
+            </Card>
+
             <Card titulo="#4 Desafio aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
             </Card>
---
+++ fundamentos-react/src/components/basicos/Familia.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default props => {
+
+    return (
+        <div>
+            {
+                props.children.map( (child, index) => {
+                    return React.cloneElement(child, {...props, key: index})
+                })
+            }
+        </div>
+    )
+
+};
